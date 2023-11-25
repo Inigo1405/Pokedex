6ba6f36fe304348dead12f074e5f5b5afe0360ad
DavidBo9 <194228@iberopuebla.mx>
2023-11-25 16:33:14 -0600

    Added auth

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -6,12 +6,7 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAw3JaohuviQMjwuRf0hanQ62u0v4AmVOM",
-  authDomain: "pokedex-9a748.firebaseapp.com",
-  projectId: "pokedex-9a748",
-  storageBucket: "pokedex-9a748.appspot.com",
-  messagingSenderId: "337521747157",
-  appId: "1:337521747157:web:334fb16fe02106cf092e5c"
+
 });
 
 
