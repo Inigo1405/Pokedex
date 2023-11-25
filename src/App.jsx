46f204a52ba0d32968f39beea1220771c5bb0290
Iñigo Quintana <90921736+Inigo1405@users.noreply.github.com>
2023-11-24 21:18:08 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import './App.css'
-import reactLogo from './assets/react.svg'
-import pokebola from './assets/pokebola.png'
-import { Pokedex } from './components/Pokedex'
+import { useState, useEffect } from 'react'
+import { Header } from './components/navbar'
+import axios from "axios";
+
 
 function App() {
+  const [page, setPage] = useState(1)
 
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${(page-1)*20}`
+
+  // Consume la API de pokemon
+  useEffect(() => {
+    axios.get(url).then((response) => {
+        const pokemonList = response.data.results
+        const pokemonPromises = pokemonList.map(pokemon => {
+            return axios.get(pokemon.url)
+        })
+
+        Promise.all(pokemonPromises).then(pokemonResponses => {
+            const pokemonData = pokemonResponses.map(res => {
+                const pokemon = res.data 
+                return {
+                    ...pokemon,
+                    sprites: pokemon.sprites
+                }
+            })
+            setPokemons(pokemonData)
+        })
+    })
+  }, [setPokemons, page])
+
+  
   return (
     <>
-      <div>
-        <a href="https://pokeapi.co/" target="_blank">
-          <img src={pokebola} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Pokemon + React</h1>
-      <div className="card">
-        <Pokedex/>
-      </div>
+      <Header/>
     </>
   )
 }
 
 export default App
+
