3fd95e4c79b476193fa101c7e111e9d0cae69585
Iñigo Quintana <quintanadinigo@gmail.com>
2023-11-26 16:21:45 -0600

    Start pokeCard

--- src/components/pokeCard.jsx
+++ src/components/pokeCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export const PokeCard = ({page, pokemon}) => {
-    // console.log(pokemon)
+    console.log(pokemon)
 
     return (
         <>
             <button onClick={() => page(3)} className="bg-black hover:bg-gray-700 text-white py-2 px-8 border rounded-full">Back</button>
-            <p>{pokemon.name}</p>
+            <h2>{pokemon.name}</h2>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} className='items-center mb-8 w-80'/>
         </>
     )
 }
