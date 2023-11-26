77ed03649e1d453382338c31d005443c2b87cf89
Iñigo Quintana <quintanadinigo@gmail.com>
2023-11-25 23:01:06 -0600

    pokemon spaces

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,24 @@ function App() {
 
   const url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${(page-1)*20}`
 
-  // Consume la API de pokemon
+  // Consume la pokeAPI
   useEffect(() => {
     axios.get(url).then((response) => {
-        const pokemonList = response.data.results
-        const pokemonPromises = pokemonList.map(pokemon => {
-            return axios.get(pokemon.url)
-        })
+      const pokemonList = response.data.results
+      const pokemonPromises = pokemonList.map(pokemon => {
+        return axios.get(pokemon.url)
+      })
 
-        Promise.all(pokemonPromises).then(pokemonResponses => {
-            const pokemonData = pokemonResponses.map(res => {
-                const pokemon = res.data 
-                return {
-                    ...pokemon,
-                    sprites: pokemon.sprites
-                }
-            })
-            setPokemons(pokemonData)
+      Promise.all(pokemonPromises).then(pokemonResponses => {
+        const pokemonData = pokemonResponses.map(res => {
+          const pokemon = res.data 
+          return {
+            ...pokemon,
+            sprites: pokemon.sprites
+          }
         })
+        setPokemons(pokemonData)
+      })
     })
   }, [setPokemons, page])
 
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -18,10 +18,6 @@ export const Pokedex = () => {
     //         poke1: {
     //             name: 'bulbasaur',
     //             sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-    //         },
-    //         poke2: {
-    //             name: 'ivysaur',
-    //             sprite: 'xd'
     //         }
     //     };
     //     setDoc(refFirebase, docData)
@@ -38,27 +34,6 @@ export const Pokedex = () => {
         })
     }
     listenToADocument()
-    
-
-    useEffect(() => {
-        axios.get(url).then((response) => {
-            const pokemonList = response.data.results
-            const pokemonPromises = pokemonList.map(pokemon => {
-                return axios.get(pokemon.url)
-            })
-
-            Promise.all(pokemonPromises).then(pokemonResponses => {
-                const pokemonData = pokemonResponses.map(res => {
-                    const pokemon = res.data 
-                    return {
-                        ...pokemon,
-                        sprites: pokemon.sprites
-                    }
-                })
-                setPokemons(pokemonData)
-            })
-        })
-    }, [setPokemons])
 
 
     return(
