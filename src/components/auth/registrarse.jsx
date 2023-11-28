b5d8e0199ec5e47ef30658ea22d843068ef24001
Iñigo Quintana <quintanadinigo@gmail.com>
2023-11-28 11:21:06 -0600

    Changes

--- src/components/auth/registrarse.jsx
+++ src/components/auth/registrarse.jsx
@@ -21,10 +21,6 @@ export const Sign = () => {
     <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
             <form onSubmit={signUp}>
-            <div>
-                <img src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-                    className="w-32 mx-auto" />
-            </div>
             <div className="mt-12 flex flex-col items-center">
                 <h1 className="text-2xl xl:text-3xl font-extrabold">
                     Sign up
