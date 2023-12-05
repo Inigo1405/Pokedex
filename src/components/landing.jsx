de8dbdd7c8d74028def6993b9900a7785467f196
Iñigo Quintana <quintanadinigo@gmail.com>
2023-12-05 11:17:39 -0600

    Img for vercel

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -5,7 +5,7 @@ export const Landing = ({page}) => {
     <>
         <div className="bg-gray-100">
             <div className="min-h-screen flex items-center justify-center">
-                <img src="/public/pokebola.png" className="items-center mb-8 w-60" alt="" />
+                <img src="/public/assets/pokebola.png" className="items-center mb-8 w-60" alt="" />
                 <div className="max-w-2xl w-full px-4">
                     <h1 className="text-4xl font-bold text-center mb-8">Welcome to the pokedex!</h1>
                     <div className="flex flex-col md:flex-row justify-center items-center gap-4">
