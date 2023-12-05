a80f7d3b43c924bc4da95fe9920e06f85196574e
Iñigo Quintana <quintanadinigo@gmail.com>
2023-12-05 00:52:13 -0600

    .

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseApp = initializeApp({
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+    apiKey: "AIzaSyAw3JaohuviQMjwuRf0hanQ62u0v4AmVOM",
+    authDomain: "pokedex-9a748.firebaseapp.com",
+    projectId: "pokedex-9a748",
+    storageBucket: "pokedex-9a748.appspot.com",
+    messagingSenderId: "337521747157",
+    appId: "1:337521747157:web:334fb16fe02106cf092e5c"
 });
 
 
